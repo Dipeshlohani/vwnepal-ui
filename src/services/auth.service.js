6fc5a21d17b4640691771938fac2f40df45c8218
Dipesh lohani <dipesh.lohani@hazesoft.co>
2022-07-02 21:37:55 +0545

    updated production url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://vwnepal-backend.herokuapp.com/auth"
+const API_URL = "https://vwnepal-backend.herokuapp.com/auth/"
 // const API_URL = "http://localhost:3600/auth/";
 
 const register = (username, email, password) => {
