c905c64815ad1245e8b185aad338df3e09c41ef6
Dipesh lohani <dipesh.lohani@hazesoft.co>
2022-09-08 14:03:00 +0545

    setup proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware(["/", , "/document", "/document/upload","/document/dialogflow/add"], { target: "https://vwnepal-backend.herokuapp.com" })
+    );
+};
