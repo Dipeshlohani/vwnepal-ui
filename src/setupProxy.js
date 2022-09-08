dbbdf77a3f0228bfcb8ef92bbb6417e62e1a1bfe
Dipesh lohani <dipesh.lohani@hazesoft.co>
2022-09-08 14:14:59 +0545

    minor comma fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware(["/", , "/document", "/document/upload","/document/dialogflow/add"], { target: "https://vwnepal-backend.herokuapp.com" })
+        createProxyMiddleware(["/", "/document", "/document/upload", "/document/dialogflow/add"], { target: "https://vwnepal-backend.herokuapp.com" })
     );
 };
